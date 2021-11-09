72ab46c05f5c28cf9c21af3e9ea5aed511b53000
luliss <02lilu1bif@hft-stuttgart.de>
2021-11-09 23:18:50 +0100

    add

--- featureCode/total_word_count.py
+++ featureCode/total_word_count.py
@@ -18,3 +18,21 @@ def clean_up_punctuation(tokens):
 def total_word_count(input: str):
     "count all words in post disregarding punctuation"
     return len(clean_up_punctuation(word_tokenize(input)))
+
+def average_word_length(input: str):
+    tokens_cleaned=clean_up_punctuation(word_tokenize(input))
+    length_sum=0
+    token_count=0
+    for t in tokens_cleaned:
+        length_sum+=len(t)
+        token_count+=1
+    return length_sum/token_count
+
+def max_word_length(input: str):
+    tokens_cleaned=clean_up_punctuation(word_tokenize(input))
+    max_length=0
+    for t in tokens_cleaned:
+        l=len(t)
+        if(l>max_length):
+            l=max_length
+    return max_length
