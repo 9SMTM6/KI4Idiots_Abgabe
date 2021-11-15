3b5882cac7c9982d7523a6b93e42d6d6129a1ec5
Samuel Maier <samuel.maier2@hotmail.de>
2021-11-15 22:03:38 +0100

    Fix last commit

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def main():
         "relativeWordCountByRelativeCount": [
             *relativeWordDensityFor(getWordsOf(
                 rankings["relativeWordCount"],
-                takeToByCat=40,
+                takeToByCat=70,
             )),
         ],
         "samuelsSelectionNaiveLog40": [
